d7567fe6f1d7acacb02d3911e3429b9e88e206e4
Rebecca Anne Sealfon <ras2198@columbia.edu>
2013-10-07 22:43:51 -0400

    Create empty Rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CitiZens::Application.config.secret_key_base = '79d8b6defd6a4ae5e43a9e7ce7ebc477eb1cb6280d1f7d97d33821d2eced65b35b0018d156b43f098f331e6e83d3861b8e4783a0dd5cbb4f2ff664f1deeef946'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+CitiZens::Application.config.session_store :cookie_store, key: '_citi-zens_session'
